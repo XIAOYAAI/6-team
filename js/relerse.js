81649fb7096cfb798f9427de41868a693d4ad7ff
XIAOYA <1505707877@qq.com>
2019-05-28 19:37:57 +0800

    1

---
+++ js/relerse.js
@@ -0,0 +1,61 @@
+//上传照片
+var imgStr = [];
+    $('#myUpload').upload({
+        uploadPath:'php/upload.php',
+        isMulti:true,
+        width:'2.7rem',
+        height:'2.7rem',
+        callback:function(data){
+            imgStr.push(data)
+        }
+});
+$('#nei').on('click',function(){
+	console.log('duan')
+	$(this).html('')
+})
+$('.btn').on('click',function(){
+  if (localStorage.sjh!=undefined) {
+  if ($('#nei').html()=='') {
+      var time = new Date();
+    imgStr= imgStr.join('-');
+     console.log(imgStr)
+    console.log('chenggong')
+   $.ajax({
+      url:'mvc/index.php?c=Login&a=regi',
+      type:'post',
+      data:{
+        neirong:$('#nei').val(),
+        shouji:localStorage.sjh,
+        img:imgStr,
+        time:formatDateTime(time)
+      },success:function(data){
+        var datas = JSON.parse(data)
+        console.log(datas)
+        if (datas.code=='200') {
+            alert('发布成功')
+            window.location.href='rji2.html'
+        };
+      }
+   })
+  }else{
+    alert('内容不能为空')
+  }
+  }else{
+    alert('请先登录');
+    window.location.href = 'index.html'
+  }
+})
+var formatDateTime = function (date) {  
+                var y = date.getFullYear();  
+                var m = date.getMonth() + 1;  
+                m = m < 10 ? ('0' + m) : m;  
+                var d = date.getDate();  
+                d = d < 10 ? ('0' + d) : d;  
+                var h = date.getHours();  
+                h=h < 10 ? ('0' + h) : h;  
+                var minute = date.getMinutes();  
+                minute = minute < 10 ? ('0' + minute) : minute;  
+                var second=date.getSeconds();  
+                second=second < 10 ? ('0' + second) : second;  
+                return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;  
+            };
---
+++ mvc/LoginController.class.php
@@ -0,0 +1,102 @@
+<?php
+/*
+ * 登录控制器
+ */
+class LoginController{
+    //上传留言板
+    public function regi(){
+        $data = $_POST;
+        //$biao = $data['biao'];
+        $shouji = $data["shouji"];
+        $neirong = $data['neirong'];
+        $img = $data['img'];
+        $old_data = M()->query_sql("SELECT * FROM liu WHERE shouji='{$shouji}'");
+        $old_data = current($old_data);
+        $result = M()->add('liu',$data);
+        if ($result) {
+            echo ajax_return('200','插入成功','');
+            exit;
+        }else{
+            echo ajax_return('404','插入失败','');
+            exit;
+        }
+    }
+    //查看留言板
+    public function query(){
+        $data = $_POST;
+        $shouji = $data['shouji'];
+        $old_data = M()->query_sql("SELECT * FROM liu WHERE shouji='{$shouji}'");
+        //$old_data = current($old_data);//current()函数返回数组中的当前元素的值。
+        $old_data1 = M()->query_sql("SELECT * FROM data WHERE shouji='{$shouji}'");
+        $old_data1 = current($old_data1);
+       /* $arr =[];
+        while(i<=$old_data1){
+            $arr[i]->$old_data1 = current($old_data1);
+        };*/
+        if (!empty($old_data)) {//empty()函数用于检查一个变量是否为空。
+            echo ajax_return('505',$old_data,$old_data1);
+            exit;   
+        }else{
+            echo ajax_return('404','用户不存在','');
+            exit; 
+        }
+    }
+    
+
+ /*
+     * 插入方法
+     */
+    public function register(){
+            $data = $_POST;
+            $shouji = $data["shouji"];
+            $name=$data["name"];
+            $neirong=$data["neirong"];
+            $img=$data["img"];
+            $old_data = M()->query_sql("SELECT * FROM data WHERE shouji='{$shouji}'");
+            $old_data = current($old_data);
+            $result = M()->add("data", $data);
+                if ($result) {
+                    echo ajax_return("200", "插入成功", "");
+                    exit;
+                } else {
+                    echo ajax_return("404", "插入失败", "");
+                    exit;
+                }
+        }
+   
+//查询数据库
+    public function querysj(){
+        $data = $_POST;
+        $shouji = $data["shouji"];
+        $old_data= M()->query_sql("SELECT * FROM data WHERE shouji='{$shouji}'");
+        $old_data= current($old_data);
+        if (!empty($old_data)) {
+            echo ajax_return("505", "用户名已存在", "");
+            exit;
+        }
+    }
+ 
+ /*
+     * 搜索方法
+     */
+    public function sousuo(){
+        $data = $_POST;
+        $shouji = $data["shouji"];
+        $old_data = M()->query_sql("SELECT * FROM data WHERE shouji='{$shouji}'");
+        $counta=count($old_data);
+        echo ajax_return($counta,'',$old_data);
+    }
+   /*
+     * 修改方法
+     */
+    public function update(){
+        $data = $_POST;
+        $id = $data["id"];
+        $shouji=$data["shouji"];
+        $name=$data["name"];
+        $neirong=$data["neirong"];
+        $img=$data["img"];
+        $result=M()->update("data",Array('shouji'=>"$shouji",'name'=>"$name",'neirong'=>"$neirong",'img'=>"$img"),$id);
+        echo "1";
+    }
+}
